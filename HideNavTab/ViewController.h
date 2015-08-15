99e7cdab11fa4c57ab7c55c126fea9ee91017e84
冰琳 <binglin@uoko.com>
2015-08-15 08:31:22 +0800

    Initial Commit

---
+++ HideNavTab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HideNavTab
+//
+//  Created by 郑林琴 on 15/8/15.
+//  Copyright (c) 2015年 BL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
